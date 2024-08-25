546193f058a2590fad7f336b4b25735ea389587f
Anirudh Jwala <anirudhsai2@gmail.com>
2024-08-25 16:34:34 +0530

    feat: Add database configuration and utility functions. Installed necessary dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,51 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      textColor: {
+        primary: "var(--gray-12)",
+        secondary: "var(--gray-11)",
+        tertiary: "var(--gray-9)",
+        brand: "var(--brand)",
+        link: "var(--blue-10)",
+      },
+      backgroundColor: {
+        primary: "var(--gray-1)",
+        secondary: "var(--gray-4)",
+        secondaryA: "var(--gray-a4)",
+        tertiary: "var(--gray-3)",
+      },
+      borderColor: {
+        primary: "var(--gray-6)",
+        secondary: "var(--gray-4)",
+      },
+      ringOffsetColor: {
+        primary: "var(--gray-12)",
+      },
+      keyframes: {
+        in: {
+          "0%": { transform: "translateY(18px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "in-reverse": {
+          "0%": { transform: "translateY(-18px)", opacity: "0" },
+          "100%": { transform: "translateY(0px)", opacity: "1" },
+        },
+      },
+      animation: {
+        in: "in .6s both",
+        "in-reverse": "in-reverse .6s both",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
+
 export default config;
