3d1faedf9dbee78b88f8bc9a6955c49880288b48
Anirudh Jwala <anirudhsai2@gmail.com>
2024-09-30 02:48:06 +0530

    refactor: Update home page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 import { RoughNotation } from "react-rough-notation";
-
 import anirudhImage from "../public/anirudh.png";
-import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]'>
       <nav className='flex justify-between items-center p-4 w-full'>
         <div className='ml-auto'>
           <ModeToggle />
@@ -24,19 +23,17 @@ export default function Home() {
         <h1 className='text-4xl xl:text-5xl font-semibold'>Anirudh Jwala</h1>
         <ol className='list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
           <li className='mb-2'>
-            I work on all things{"\u00A0"}
+            I work on all things{" "}
             <RoughNotation type='underline' color='#df8c4a' show>
               Frontend
-            </RoughNotation>
-            {"\u00A0"}at <br className='block md:hidden' />
-            <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold'>
-              IRIS Software Group
-            </code>
+            </RoughNotation>{" "}
+            at <br className='block md:hidden' />
+            IRIS Software
           </li>
           <li className='mb-2'>Truly love working on Web</li>
         </ol>
 
-        <div className='flex gap-4 items-center flex-col sm:flex-row'>
+        <div className='flex gap-4 items-center flex-row'>
           <a
             className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
             href='https://github.com/jwala-anirudh'
@@ -70,36 +67,6 @@ export default function Home() {
         </div>
       </main>
       <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
-        {/* <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='https://nextjs.org/icons/window.svg'
-            alt='Window icon'
-            width={16}
-            height={16}
-          />
-          Examples
-        </a> */}
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://blog.learncodeonline.in/razorpay-integration-in-react'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='https://nextjs.org/icons/file.svg'
-            alt='File icon'
-            width={16}
-            height={16}
-          />
-          latest blog
-        </a>
         <a
           className='flex items-center gap-2 hover:underline hover:underline-offset-4'
           href='https://x.com/nirudhuuu'
@@ -116,6 +83,21 @@ export default function Home() {
           <RoughNotation type='highlight' color='#df8c4a' show>
             read tweets
           </RoughNotation>
+        </a>
+        <a
+          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
+          href='https://blog.learncodeonline.in/razorpay-integration-in-react'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Image
+            aria-hidden
+            src='https://nextjs.org/icons/file.svg'
+            alt='File icon'
+            width={16}
+            height={16}
+          />
+          latest blog
         </a>
       </footer>
     </div>
