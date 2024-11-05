1ac05623caace8c21c5ba052817eb013f1052714
Anirudh Jwala <anirudhsai2@gmail.com>
2024-11-06 01:45:17 +0530

    feat: refactor ExperienceSection to use a dynamic experiences array for improved maintainability

--- components/experience-section.tsx
+++ components/experience-section.tsx
@@ -12,67 +12,61 @@ import capgeminiLogo from "@/public/svg/capgemini.svg";
 import sportsVisionLogo from "@/public/svg/sportsvision.svg";
 
 const ExperienceSection = () => {
+  const experiences = [
+    {
+      id: 1,
+      company: "IRIS Software Group",
+      logo: irisLogo,
+      position: "Software Engineer",
+      startDate: "Sep 2023",
+      endDate: "Present",
+      duration: "1 yr 3 mos",
+    },
+    {
+      id: 2,
+      company: "Capgemini",
+      logo: capgeminiLogo,
+      position: "Senior Software Engineer",
+      startDate: "Mar 2021",
+      endDate: "Sep 2023",
+      duration: "2 yrs 7 mos",
+    },
+    {
+      id: 3,
+      company: "Sports Vision",
+      logo: sportsVisionLogo,
+      position: "Software Engineer",
+      startDate: "Oct 2018",
+      endDate: "Feb 2021",
+      duration: "2 yrs 5 mos",
+    },
+  ];
+
   return (
     <section>
       <h2 className='text-xl font-semibold text-left mb-3'>Experience</h2>
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-none lg:flex lg:flex-wrap mb-8'>
-        {/* IRIS Software Group */}
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-sm'>
-            <Image
-              src={irisLogo}
-              alt='IRIS Software Group'
-              className='mb-2 w-10 h-10 object-contain'
-            />
-            IRIS Software Group
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-1 text-sm font-semibold text-zinc-700'>
-              Software Engineer
-            </CardDescription>
-            <CardDescription className='pb-3 text-sm'>
-              Sep 2023 - Present <br /> 1 yr 3 mos
-            </CardDescription>
-          </CardContent>
-        </Card>
-        {/* Capgemini */}
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-sm'>
-            <Image
-              src={capgeminiLogo}
-              alt='Capgemini'
-              className='mb-2 w-6 h-10 object-contain'
-            />
-            Capgemini
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-1 text-sm font-semibold text-zinc-700'>
-              Senior Software Engineer
-            </CardDescription>
-            <CardDescription className='pb-3 text-sm'>
-              Mar 2021 - Sep 2023 <br /> 2 yrs 7 mos
-            </CardDescription>
-          </CardContent>
-        </Card>
-        {/* Sports Vision */}
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-sm'>
-            <Image
-              src={sportsVisionLogo}
-              alt='Sports Vision'
-              className='mb-2 w-10 h-10 object-contain'
-            />
-            Sports Vision
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-1 text-sm font-semibold text-zinc-700'>
-              Software Engineer
-            </CardDescription>
-            <CardDescription className='pb-3 text-sm'>
-              Oct 2018 - Feb 2021 <br /> 2 yrs 5 mos
-            </CardDescription>
-          </CardContent>
-        </Card>
+        {experiences.map((experience) => (
+          <Card key={experience.id} className='rounded-2xl w-full lg:w-48'>
+            <CardHeader className='pb-0 text-sm'>
+              <Image
+                src={experience.logo}
+                alt={experience.company}
+                className='mb-2 w-10 h-10 object-contain'
+              />
+              {experience.company}
+            </CardHeader>
+            <CardContent>
+              <CardDescription className='pb-1 text-sm font-semibold text-zinc-700'>
+                {experience.position}
+              </CardDescription>
+              <CardDescription className='pb-3 text-sm'>
+                {experience.startDate} - {experience.endDate} <br />{" "}
+                {experience.duration}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
