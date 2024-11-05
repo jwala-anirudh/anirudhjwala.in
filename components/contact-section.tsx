12dc3916881180d62efbd72af93977dc06835e85
Anirudh Jwala <anirudhsai2@gmail.com>
2024-11-06 01:45:13 +0530

    feat: refactor ContactSection to use a dynamic contacts array for improved maintainability

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -12,62 +12,51 @@ import phoneLogo from "@/public/svg/phone.svg";
 import addressLogo from "@/public/svg/address.svg";
 
 const ContactSection = () => {
+  const contacts = [
+    {
+      id: 1,
+      title: "Email",
+      description: "hi@anirudhjwala.in",
+      logo: emailLogo,
+    },
+    {
+      id: 2,
+      title: "Address",
+      description:
+        "3-6-750/6, Fourth Floor, Mataru Chaya, Landmark: Opposite Karur Vysya Bank, Hyderabad",
+      logo: addressLogo,
+    },
+    {
+      id: 3,
+      title: "Phone Number",
+      description: "+91-7981970397",
+      logo: phoneLogo,
+    },
+  ];
+
   return (
     <section>
       <h2 className='text-xl font-semibold text-left mb-3'>Contact</h2>
       <div className='flex flex-wrap gap-2 mb-8'>
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-base font-semibold'>
-            <Image
-              src={emailLogo}
-              alt='email'
-              width={40}
-              height={40}
-              className='mb-2 w-6 h-6'
-            />
-            Email
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-3 text-sm'>
-              hi@anirudhjwala.in
-            </CardDescription>
-          </CardContent>
-        </Card>
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-base font-semibold'>
-            <Image
-              src={addressLogo}
-              alt='address'
-              width={40}
-              height={40}
-              className='mb-2 w-6 h-6'
-            />
-            Address
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-3 text-sm'>
-              3-6-750/6, Fourth Floor, Mataru Chaya, Landmark: Opposite Karur
-              Vysya Bank, Hyderabad
-            </CardDescription>
-          </CardContent>
-        </Card>
-        <Card className='rounded-2xl w-full lg:w-48'>
-          <CardHeader className='pb-0 text-base font-semibold'>
-            <Image
-              src={phoneLogo}
-              alt='phone'
-              width={40}
-              height={40}
-              className='mb-2 w-6 h-6'
-            />
-            Phone Number
-          </CardHeader>
-          <CardContent>
-            <CardDescription className='pb-3 text-sm'>
-              +91-7981970397
-            </CardDescription>
-          </CardContent>
-        </Card>
+        {contacts.map((contact) => (
+          <Card key={contact.id} className='rounded-2xl w-full lg:w-48'>
+            <CardHeader className='pb-0 text-base font-semibold'>
+              <Image
+                src={contact.logo}
+                alt={contact.title}
+                width={40}
+                height={40}
+                className='mb-2 w-6 h-6'
+              />
+              {contact.title}
+            </CardHeader>
+            <CardContent>
+              <CardDescription className='pb-3 text-sm'>
+                {contact.description}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
