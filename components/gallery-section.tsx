af4b6d2c54e796271badfb8c22fe90f47ade043e
Anirudh Jwala <anirudhsai2@gmail.com>
2025-02-22 10:24:40 +0530

    refactor: streamline GallerySection layout by removing unnecessary markup

--- components/gallery-section.tsx
+++ components/gallery-section.tsx
@@ -83,30 +83,25 @@ const GallerySection = () => {
   ];
 
   return (
-    <section className="overflow-hidden">
-      <h2 className="text-2xl font-semibold text-left mb-3 font-[family-name:var(--font-departure-mono)]">
-        Gallery
-      </h2>
-      <Swiper
-        effect="cards"
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="w-60 h-80 my-8 overflow-hidden"
-      >
-        {photos.map((photo) => (
-          <SwiperSlide
-            key={photo.id}
-            className="flex items-center justify-center rounded-lg"
-          >
-            <Image
-              src={photo.src}
-              alt={photo.alt}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
+    <Swiper
+      effect="cards"
+      grabCursor={true}
+      modules={[EffectCards]}
+      className="w-60 h-80 my-8 overflow-hidden"
+    >
+      {photos.map((photo) => (
+        <SwiperSlide
+          key={photo.id}
+          className="flex items-center justify-center rounded-lg"
+        >
+          <Image
+            src={photo.src}
+            alt={photo.alt}
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
